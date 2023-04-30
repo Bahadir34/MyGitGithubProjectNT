23e2e4f16c46f9274f43d4e521384fe69f4efa57
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-04-30 17:53:25 +0300

    v1

--- src/Account.java
+++ src/Account.java
@@ -4,6 +4,7 @@ public class Account
     public static void main(String[] args)
     {
         System.out.println("Bazi kodlar yazildi.");
+        System.out.println("That is new commit after cloned project from github.");
 
     }
 
